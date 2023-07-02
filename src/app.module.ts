69e3ac4e93c69dafb88538fff5042f5fb39bb116
cuervolu <cuervolu@protonmail.com>
2023-07-01 22:09:13 -0400

    add: connection to postgres DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ConfigModule } from '@nestjs/config';
 
 import { join } from 'path';
 import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
 
@@ -19,6 +22,16 @@ import { ItemsModule } from './items/items.module';
 
       // Este plugin es para levantar el Apollo Studio
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
     }),
     ItemsModule,
   ],
